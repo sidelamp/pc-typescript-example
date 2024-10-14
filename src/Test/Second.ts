01f557c8dd338aa0441be9788dcd239a329c5723
Михаил <gilfmuxa@gmail.com>
2024-10-14 22:03:38 +0400

    update attributes

--- src/Test/Second.ts
+++ src/Test/Second.ts
@@ -1,22 +1,30 @@
-import { TJsonAttributeSchemaProp } from "../types/attributes";
+import { TJsonAttributeSchemaProp, AttributeSchema } from "../types/attributes";
 import { ScriptTypeBase } from "../types/ScriptTypeBase";
 import { attrib, createScript } from "../utils/createScriptDecorator";
 
 @createScript("second")
 class Second extends ScriptTypeBase {
-    @attrib({
-        type: "json",
-        schema: [
-            {
-                name: "kek",
-                type: "string",
-                default: "loh",
-            }]
-    })
+    @attrib(
+        {
+            type: AttributeSchema.json,
+            schema: [
+                {
+                    name: "prop",
+                    type: AttributeSchema.string,
+                    default: ["first", "second", "thrid"],
+                    array: true,
+                },
+                {
+                    name: "prop2",
+                    type: AttributeSchema.number,
+                    default: 1
+                }
+            ]
+        })
     property: TJsonAttributeSchemaProp;
 
     initialize(): void {
-        console.log(this.property);
+        console.log((this.property as any)["prop2"]);
     }
 }
 
